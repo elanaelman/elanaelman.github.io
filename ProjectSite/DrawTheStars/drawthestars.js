8e5205a62ca7de8ae475a38d57a42c692d742180
TooManySardines <elana.elman@gmail.com>
2020-05-08 16:02:00 -0400

    updated headers
    quick fix

--- ProjectSite/DrawTheStars/drawthestars.js
+++ ProjectSite/DrawTheStars/drawthestars.js
@@ -1,3 +1,5 @@
 function roundOff(n, precision) {
   return Math.round(n*Math.pow(10, precision))/Math.pow(10, precision);
 }
+
+document.getElementById("header").innerHTML += "<span id='header-right'><b><a href='drawthestars-simple.html'>Draw a star?</a> || <a href='drawthestars-req.html'>Draw a bunch of stars?</a></b></span>";
--- ProjectSite/site.js
+++ ProjectSite/site.js
@@ -1,3 +1,1 @@
-document.getElementById("header").innerHTML = "<a href='../index.html' id='home' class='fancy'>Coding Adventures</a><span id='header-right'><b><a href='drawthestars-simple.html'>Draw a star?</a> || <a href='drawthestars-req.html'>Draw a bunch of stars?</a> || <a href='drawthestars-search.html'>Draw a ton of stars?</a></b></span>";
-
-document.getElementById("footer").innerHTML = "<div>Website footer, thanks for visiting.</div>";
+document.getElementById("header").innerHTML = "<a href='../index.html' id='home' class='fancy'>Coding Adventures</a>"
